2b804a5cabf50fe59c9a15dbce803bacb9dbd948
Tsengo <niktsenguashvili@gmail.com>
2022-10-23 20:46:01 +0400

    my cv

---
+++ script/script.js
@@ -0,0 +1,6 @@
+const links = [
+    'www.linkedin.com/in/nika-tsenguashvili-6a090b238',
+    'https://www.facebook.com/nika.tsengo',
+    'https://www.instagram.com/nikatsengo/',
+    'https://profile.indeed.com/?hl=en&co=US&from=gnav-passport--passport-webapp&_ga=2.234777243.466022112.1666531742-715773071.1666024638',
+]
