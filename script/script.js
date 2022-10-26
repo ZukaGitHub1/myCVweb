91388d09f1c83e2f36db70a916671589659dded1
Tsengo <niktsenguashvili@gmail.com>
2022-10-26 21:51:22 +0400

    #

--- script/script.js
+++ script/script.js
@@ -1,6 +1,36 @@
-const links = [
-    'www.linkedin.com/in/nika-tsenguashvili-6a090b238',
-    'https://www.facebook.com/nika.tsengo',
-    'https://www.instagram.com/nikatsengo/',
-    'https://profile.indeed.com/?hl=en&co=US&from=gnav-passport--passport-webapp&_ga=2.234777243.466022112.1666531742-715773071.1666024638',
-]
+// const webflows = [
+//     {
+//         img:'<img src="img/Boldo.PNG">',
+//         link:'https://boldo.pages.dev/',
+//     },
+//     {
+//         img:'<img src="img/chat-app.PNG">',
+//         link:'https://chat-web-czt.pages.dev/',
+//     },
+//     {
+//         img:'<img src="img/colloseum.PNG">',
+//         link:'https://colloseumgym.pages.dev/',
+//     },
+//     {
+//         img:'<img src="img/findtrend.PNG">',
+//         link:'https://findtrend.pages.dev/',
+//     },
+//     {
+//         img:'<img src="img/product.PNG">',
+//         link:'https://product-preview-card-eu4.pages.dev/',
+//     },
+//     {
+//         img:'<img src="img/single-page.PNG">',
+//         link:'https://single-page-web.pages.dev/',
+//     },
+// ]
+
+// webflows.forEach(element=>{
+//     let website = document.getElementById('webs')
+//     let aLink = document.createElement('a');
+//     let divs = document.createElement('div');
+//     divs.setAttribute('class','flows');
+//     aLink.textContent = element.link;
+//     divs.textContent = element.img;
+//     website.appendChild(divs);
+// })
