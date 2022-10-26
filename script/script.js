92962d99ba042388b6fa244f10cc9238f642bc34
Zukkaaa <89594950+ZukaGitHub1@users.noreply.github.com>
2022-10-26 22:15:13 +0400

    animation
    animation skills  with js

--- script/script.js
+++ script/script.js
@@ -34,3 +34,25 @@
 //     divs.textContent = element.img;
 //     website.appendChild(divs);
 // })
+
+
+
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".skills");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().left;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
